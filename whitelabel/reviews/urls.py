d42143ea1819aa27b3b36950e6d55dcb29704071
Jamie Gullbrand <jamiegullbrand@gmail.com>
2019-04-01 09:43:36 -0400

    load templates for software categories / software products pages

--- whitelabel/reviews/urls.py
+++ whitelabel/reviews/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'), #home page
-    path('categories/', views.categories, ), #will software products based on category
+    path('categories/', views.categories, name = 'categories'), #list of categories
+    path('software_products/', views.software_products, name = 'software_products'),
     path('profile/', views.profile, name = 'profile')
 ]
--- whitelabel/reviews/views.py
+++ whitelabel/reviews/views.py
@@ -10,6 +10,10 @@ def categories(request):
 	context = {} # will update later
 	return render(request, 'reviews/categories.html', context)
 
+def software_products(request):
+	context = {} # will update later
+	return render(request, 'reviews/software_products.html', context)	
+
 def profile(request):
 	context = {} # will update later
 	return render(request, 'reviews/profile.html', context)	
